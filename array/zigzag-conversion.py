0f96e87734296068638e99c038ac8f5a97627f0d
tinhtt <tinhtrantrung.vn@gmail.com>
2025-05-05 21:10:23 +1200

    Update.

---
+++ array/zigzag-conversion.py
@@ -0,0 +1,57 @@
+# https://leetcode.com/problems/zigzag-conversion/?envType=study-plan-v2&envId=top-interview-150
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# And then read line by line: "PAHNAPLSIIGYIR"
+
+# Write the code that will take a string and make this conversion given a number of rows:
+
+# string convert(string s, int numRows);
+
+
+# Example 1:
+
+# Input: s = "PAYPALISHIRING", numRows = 3
+# Output: "PAHNAPLSIIGYIR"
+# Example 2:
+
+# Input: s = "PAYPALISHIRING", numRows = 4
+# Output: "PINALSIGYAHRPI"
+# Explanation:
+# P     I    N
+# A   L S  I G
+# Y A   H R
+# P     I
+# Example 3:
+
+# Input: s = "A", numRows = 1
+# Output: "A"
+
+
+# Constraints:
+
+# 1 <= s.length <= 1000
+# s consists of English letters (lower-case and upper-case), ',' and '.'.
+# 1 <= numRows <= 1000
+
+
+def zigzag_conversion(s: str, numRows: int) -> str:
+    if numRows == 1:
+        return s
+
+    rows = [""] * numRows
+    going_down = False
+    i = 0
+    for c in s:
+        if i == 0 or i == numRows - 1:
+            going_down = not going_down
+
+        rows[i] += c
+        if going_down:
+            i += 1
+        else:
+            i -= 1
+
+    return "".join(rows)
